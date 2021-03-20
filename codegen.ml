48b87b1616bc6f1a403c05ee6db81fde9afd7d3b
DesuImudia <56054713+DesuImudia@users.noreply.github.com>
2021-03-19 22:23:54 -0400

    Comment out non-tyoes

--- codegen.ml
+++ codegen.ml
@@ -11,19 +11,19 @@ let translate (globals, functions) =
   
   (* Create the LLVM compilation module into which
      we will generate code *)
-  let the_module = L.create_module context "MicroC" in
+  let the_module = L.create_module context "MatrixMania" in
 
   (* Get types from the context *)
   let i32_t      = L.i32_type    context
-  and i8_t       = L.i8_type     context
-  and i1_t       = L.i1_type     context
+  (* and i8_t       = L.i8_type     context
+  and i1_t       = L.i1_type     context *)
   and float_t    = L.double_type context
   and void_t     = L.void_type   context in
 
   (* Return the LLVM type for a MicroC type *)
   let ltype_of_typ = function
       A.Int   -> i32_t
-    | A.Bool  -> i1_t
+   (* | A.Bool  -> i1_t *)
     | A.Float -> float_t
     | A.Void  -> void_t
   in
