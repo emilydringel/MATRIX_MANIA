da078e5a4cbf824d435c718e590e5dc967b958a4
Cindy Espinosa <cje2127@barnard.edu>
2021-04-04 10:28:09 -0400

    added another print

--- tests/gcd.mm
+++ tests/gcd.mm
@@ -1,14 +1,15 @@
 int gcd(int x, int y){ 
  while(x != y){
-  if(x > y) 
-   x = x - y;
-  else
-   y = y - x;
+   if(x > y) 
+      x = x - y;
+   else
+      y = y - x;
 }
 return x;
 }
 
 def main ( ){
- printf(gcd(3, 15));
+ print(gcd(3, 15));
+ print(gcd(18,24));
  return 0;
 }
