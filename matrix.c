1870db938678372a2d9ca4579eabb779f32f1d16
Cindy Espinosa <cje2127@barnard.edu>
2021-04-04 12:52:11 -0400

    matrix.c

--- matrix.c
+++ matrix.c
@@ -1,6 +1,26 @@
 (* built-in functions *)
+
+(* size *)
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+static void die(const char *m){
+ perror(m);
+ exit(1);
+}
+
+struct matrix{
+ int n_rows;
+ int n_columns;
+}
+
+matrix* size(matrix* mat){
+ int size[] = {mat->n_rows, mat->n_columns};
+ return size; 
+ 
+ 
+}
 
 
