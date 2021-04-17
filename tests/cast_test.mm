418fb7e625cce25de406798b3b4881ec15f998fd
emilydringel <edr2124@barnard.edu>
2021-04-17 10:56:26 -0400

    fixed semant return + semant tests

---
+++ tests/cast_test.mm
@@ -0,0 +1,7 @@
+/* Semant Check -- float = int */
+
+def int main(){
+    int x = 0;
+    float y = x; /* Should throw an error */
+    print(y);
+}
---
+++ tests/return_test.mm
@@ -0,0 +1,11 @@
+/* Semant Check -- return test */
+
+def int retFive(){
+    int x = 5;
+    return x;
+}
+
+def int main(){
+    int x = retFive();
+    print(x);
+}
