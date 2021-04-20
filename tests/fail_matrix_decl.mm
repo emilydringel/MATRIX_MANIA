a777ac6cfb86493e286034d895a8014bd8f10f82
Cindy Espinosa <cje2127@barnard.edu>
2021-04-20 19:39:30 -0400

    .out file for transpose

--- tests/fail_matrix_decl.mm
+++ tests/fail_matrix_decl.mm
@@ -1,4 +1,4 @@
-def int main( ){
+def int main(){
 	matrix< > x = [1];
 }
 
--- tests/test_transpose.mm
+++ tests/test_transpose.mm
@@ -1,14 +1,14 @@
 def matrix<int> transpose(matrix<int> m, matrix<int> n){
  matrix<int> rowscols = size(m);
 
- for(int i = 0; i < rowscols[0,0]; i++){
-  for(int j = 0; j < rowscols[1,0]; j++)
+ for(int i = 0; i < rowscols[0,0]; i+1){
+  for(int j = 0; j < rowscols[1,0]; j+1)
 	n[i,j] = m[j,i];  
 }
  return n;
 }
 
-def int main( ){
+def int main(){
 
 matrix<int> m = [1,2,3; 4,5,6];
 matrix<int> n = [0,0;0,0;0,0];
@@ -20,5 +20,4 @@ matrix<int> tranposed = transpose(m,m);
 
  printm(transpose([7,8,9,10; 11,12,13,14]);
  printm(transpose([7,8,9,10; 11,12,13,14],[0,0;0,0;0,0;0,0]);
- return 0;
 } 
