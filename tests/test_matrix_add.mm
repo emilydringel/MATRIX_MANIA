10f9ba5fb3e3fbc1efbaac7ac09a00023b45644b
Cindy Espinosa <cje2127@barnard.edu>
2021-04-20 16:48:58 -0400

    matrix addition

--- tests/test_matrix_add.mm
+++ tests/test_matrix_add.mm
@@ -1,1 +1,26 @@
+def matrix<int> add(matrix<int> x, matrix<int> y){
 
+matrix<int> rowscols = size(x);
+matrix<int> empty = [0,0];
+
+for(int i = 0; i < rowscols[0,0]; i++)
+  for(int j = 0; j < rowscols[1,0]; j++){
+    empty[i,j] = x[i,j]+y[i,j];
+  }
+  return empty;
+}
+
+
+def int main( ){
+  
+matrix<int> m = [1,4];
+matrix<int> n = [2,3];
+
+matrix<int> added = add(m,n);
+
+  printm(m);
+  printm(n);
+  printm(added);
+  printm(add([6,8; 9,10]));
+  return 0; 
+}
