6bab7e7b2ec72c45a3ad83ae6e6f34eee55b0b2d
sreesewirpsa <sophie.reesewirpsa@gmail.com>
2021-04-20 10:25:03 -0400

    fixed to match our language syntax

--- tests/gcd.mm
+++ tests/gcd.mm
@@ -1,19 +1,19 @@
-def int main ( ){
+/*gcd program to showcase our language's ability to compute common cs problems */
 
 def int gcd(int x, int y){ 
- if (x == 0){
-   return y;
+   if (x == 0){
+      return y;
+   }
+   while(x != y){
+      if(x > y) {x = x - y;}
+      else {y = y - x;}
+   }
+   return x;
 }
- while(x != y){
-   if(x > y) 
-      x = x - y;
-   else
-      y = y - x;
-}
-return x;
-}
- print(gcd(3, 15));
- print(gcd(18,24));
- print(gcd(45,120));
- return 0;
+
+def int main( ){
+   print(gcd(3, 15));
+   print(gcd(18,24));
+   print(gcd(45,120));
+   return 0;
 }
