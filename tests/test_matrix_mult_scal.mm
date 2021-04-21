95d76dd68c3376aade141690d6f143c15186c02b
Cindy Espinosa <cje2127@barnard.edu>
2021-04-21 15:26:17 -0400

    with new rows/cols

--- tests/test_matrix_mult_scal.mm
+++ tests/test_matrix_mult_scal.mm
@@ -1,23 +1,23 @@
 def matrix<int> multiply_s(matrix<int> x, int y){
+    int sizeOfR1 = getRows(x);
+    int sizeOfC1= getColumns(x);
+    matrix<int> empty = [0,0,0;0,0,0];
 
-matrix<int> rowscols = size(x);
-matrix<int> empty = [0,0,0;0,0,0];
-
-for(int i = 0; i < rowscols[0,0]; i++)
-  for(int j = 0; j < rowscols[1,0]; j++){
-    empty[i,j] = x[i,j] * y;
-  }
-  return empty;
+    for(int i = 0; i < sizeOfR1; i+1)
+  	for(int j = 0; j < sizeOfC1; j+1){
+    	    empty[i,j] = x[i,j] * y;
+  	}
+  	return empty;
 }
 
 
 def int main( ){
   
-matrix<int> m = [1,4,3;2,7,6];
-int n = 2;  
+    matrix<int> m = [1,4,3;2,7,6];
+    int n = 2;  
 
-matrix<int> multiplied = multiply_s(m,n);
+    matrix<int> multiplied = multiply_s(m,n);
 
-  printm(multiplied);
-  printm(multiply_s([6,8,9;9,10,11],2));
+    printm(multiplied);
+    printm(multiply_s([6,8,9;9,10,11],2));
 }
