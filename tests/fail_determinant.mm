e8db4ca3bb0be8ca7b355f7b94b2a7b32fdc3d81
Cindy Espinosa <cje2127@barnard.edu>
2021-04-21 15:33:12 -0400

    fail for determinant and .err

---
+++ tests/fail_determinant.mm
@@ -0,0 +1,9 @@
+def int det(matrix<int> x){
+    return (x[0,0] * x[1,1] - x[0,1] * x[1,0]);
+}
+def int main( ){
+        matrix<float> m = [1,2;3,4];      
+        int d = det(m);
+        print(d);       
+}
+
