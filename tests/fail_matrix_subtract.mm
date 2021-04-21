590f12dab227b499a1ef6ba03ae7d3e4ba36a1fb
Cindy Espinosa <cje2127@barnard.edu>
2021-04-21 11:58:31 -0400

    fail test matrix subtraction

---
+++ tests/fail_matrix_subtract.mm
@@ -0,0 +1,26 @@
+def matrix<int> add(matrix<int> x, matrix<int> y){
+  
+matrix<int> rowscols = size(x);
+matrix<int> empty = [0,0];
+
+for(int i = 0; i < rowscols[0,0]; i++)
+  for(int j = 0; j < rowscols[1,0]; j++){
+    empty[i,j] = x[i,j]-y[i,j];
+  }
+  return empty;
+}
+
+
+def int main( ){
+
+matrix<int> m = [1,4];
+matrix<int> n = [2,3];
+
+matrix<int> subtract = sub(m,n);
+
+  printm(m);
+  printm(n);
+  printm(subtract);
+  printm(sub([6,8,1; 9,10])); /* Fail: invalid matrix dims */
+}
+
