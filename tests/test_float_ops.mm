b9a190816a34c50e21522c0c3a3c2dc79555003e
Cindy Espinosa <cje2127@barnard.edu>
2021-04-21 10:20:36 -0400

    syntax error

--- tests/test_float_ops.mm
+++ tests/test_float_ops.mm
@@ -13,11 +13,9 @@ def void testfloat(float a, float b){
     print(a <= b);
 }
 def int main(){
-    float c;
-    float d;
 
-    c = 93.6;
-    d = 7.26;
+    float c = 93.6;
+    float d = 7.26;
 
     testfloat(c, d);
 
