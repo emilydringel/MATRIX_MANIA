c8ab8e00e1adc6dfcf2498df17a76a361776a198
Cindy Espinosa <cje2127@barnard.edu>
2021-04-21 14:52:59 -0400

    syntax issues

--- tests/test_matrix_add.mm
+++ tests/test_matrix_add.mm
@@ -1,25 +1,25 @@
 def matrix<int> add(matrix<int> x, matrix<int> y){
 
-matrix<int> empty = [0,0];
+    matrix<int> empty = [0,0];
 
-for(int i = 0; i < 2; i++){
-  for(int j = 0; j < 2; j++){
-    empty[i,j] = x[i,j]+y[i,j];
-  }
-  return empty;
+    for(int i = 0; i < 2; i++){
+    	for(int j = 0; j < 2; j++){
+            empty[i,j] = x[i,j]+y[i,j];
+        }
+  	return empty;
+    }
 }
-}
 
 
-def int main( ){
+def int main(){
   
-matrix<int> m = [1,4];
-matrix<int> n = [2,3];
+    matrix<int> m = [1,4];
+    matrix<int> n = [2,3];
 
-matrix<int> added = add(m,n);
+    matrix<int> added = add(m,n);
 
-  printm(m);
-  printm(n);
-  printm(added);
-  printm(add([6,8],[9,10]));
+    printm(m);
+    printm(n);
+    printm(added);
+    printm(add([6,8],[9,10]));
 }
