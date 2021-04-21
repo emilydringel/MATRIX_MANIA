aa3d124639b117d221a075b57cf6ad6e2a3b4a01
Cindy Espinosa <cje2127@barnard.edu>
2021-04-21 15:21:41 -0400

    new rows/cols

--- tests/test_matrix_subtract.mm
+++ tests/test_matrix_subtract.mm
@@ -1,25 +1,24 @@
 def matrix<int> add(matrix<int> x, matrix<int> y){
 
-matrix<int> rowscols = size(x);
-matrix<int> empty = [0,0];
+    matrix<int> empty = [0,0];
 
-for(int i = 0; i < rowscols[0,0]; i++)
-  for(int j = 0; j < rowscols[1,0]; j++){
-    empty[i,j] = x[i,j]-y[i,j];
-  }
-  return empty;
+    for(int i = 0; i < 2; i+1)
+  	for(int j = 0; j < 2; j+1){
+    	    empty[i,j] = x[i,j]-y[i,j];
+  	}
+  	return empty;
 }
 
 
 def int main( ){
   
-matrix<int> m = [1,4];
-matrix<int> n = [2,3];
+    matrix<int> m = [1,4];
+    matrix<int> n = [2,3];
 
-matrix<int> subtract = sub(m,n);
+    matrix<int> subtract = sub(m,n);
 
-  printm(m);
-  printm(n);
-  printm(subtract);
-  printm(sub([6,8; 9,10]));
+    printm(m);
+    printm(n);
+    printm(subtract);
+    printm(sub([6,8],[9,10]));
 }
