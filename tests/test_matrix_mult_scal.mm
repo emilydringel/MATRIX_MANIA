1440c8a395f1b4333337e982ff729c93cc8c9a72
Cindy Espinosa <cje2127@barnard.edu>
2021-04-22 19:55:13 -0400

    syntax

--- tests/test_matrix_mult_scal.mm
+++ tests/test_matrix_mult_scal.mm
@@ -2,8 +2,8 @@ def matrix<int> multiply_s(matrix<int> x, matrix<int> empty, int y){
     int sizeOfR1 = getRows(x);
     int sizeOfC1= getColumns(x);
 
-    for(int i = 0; i < sizeOfR1; i+1)
-  	for(int j = 0; j < sizeOfC1; j+1){
+    for(int i = 0; i < sizeOfR1; i=i+1)
+  	for(int j = 0; j < sizeOfC1; j=j+1){
     	    empty[i,j] = x[i,j] * y;
   	}
   	return empty;
