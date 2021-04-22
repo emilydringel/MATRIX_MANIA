b0fc5953e0ade395fc86648a7dd014824b880b46
Cindy Espinosa <cje2127@barnard.edu>
2021-04-22 17:09:43 -0400

    fixed with getRows/getCols

--- tests/test_mat_multiply.mm
+++ tests/test_mat_multiply.mm
@@ -1,18 +1,11 @@
-def matrix<int> multiply(matrix<int> x, matrix<int> y){
-/* matrix<int> empty as parameter? */
+def matrix<int> multiply(matrix<int> x, matrix<int> y, matrix<int> empty){
 
     int sizeOfR1 = getRows(x);
     int sizeOfC1 = getColumns(x);
-    int sizeOfR2 = getRows(y);
-    int sizeOfC2= getColumns(y);
-
-    matrix<int> empty = [0,0];
 
     for(int i = 0; i < sizeOfR1; i+1){
-	for(int j = 0; j < sizeOfC2; j+1){
-	    for(int k = 0; k < sizeOfC1; k+1){
-		empty[i,j]+= x[i,k]*y[k,j];
-	    }
+	for(int j = 0; j < sizeOfC1; j+1){
+	    empty[i,j]+= x[i,j]*y[i,j];
   	}
     }
   return empty;
@@ -22,6 +15,7 @@ def int main(){
   
 matrix<int> m = [1,4];
 matrix<int> n = [2,3];
+matrix<int> empty = [0,0];
 
 matrix<int> multiplied = multiply(m,n);
 
