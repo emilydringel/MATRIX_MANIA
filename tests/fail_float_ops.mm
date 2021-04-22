13afd3b0d47882f2e9f323054e35835e9bb62978
Cindy Espinosa <cje2127@barnard.edu>
2021-04-21 23:47:54 -0400

    float ops fail

---
+++ tests/fail_float_ops.mm
@@ -0,0 +1,24 @@
+def void testfloat(float a, float b){
+    printf(a + b);
+    printf(a - b);
+    printf(a * b);
+    printf(a / b);
+    print(a == b);
+    print(a == a);
+    print(a != b);
+    print(a != a);
+    print(a > b);
+    print(a >= b);
+    print(a < b);
+    print(a <= b);
+}
+def int main(){
+
+    float c = 93.6;
+    int d = 7.26;
+
+    testfloat(c, d);
+
+    testfloat(d, d);
+
+}
