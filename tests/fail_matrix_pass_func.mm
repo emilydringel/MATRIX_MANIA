9988a4a4ae8891981f981687cb6bf3b436a36d86
Cindy Espinosa <cje2127@barnard.edu>
2021-04-22 14:00:32 -0400

    added main

---
+++ tests/fail_matrix_pass_func.mm
@@ -0,0 +1,7 @@
+def int pass(matrix<int> ){ /*Fail: missing variable name */
+	matrix<int> a = [1,2];
+	matrix<int> temp = a;
+}
+def int main(){
+	printm(temp);
+}
---
+++ tests/test_matrix_pass_func.mm
@@ -0,0 +1,7 @@
+def int pass(matrix<int> a){
+	matrix<int> a = [1,2];
+	matrix<int> temp = a;
+}
+def int main(){
+	printm(temp);
+}
