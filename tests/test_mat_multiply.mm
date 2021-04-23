da8c484c701ca1db21999f027dc73a5959a3d144
sreesewirpsa <sophie.reesewirpsa@gmail.com>
2021-04-22 22:03:44 -0400

    style guide errors

--- tests/test_mat_multiply.mm
+++ tests/test_mat_multiply.mm
@@ -4,20 +4,20 @@ def matrix<int> multiply(matrix<int> x, matrix<int> y, matrix<int> empty){
     int sizeOfC1 = getColumns(x);
 
     for(int i = 0; i < sizeOfR1; i=i+1){
-	for(int j = 0; j < sizeOfC1; j=j+1){
-	    empty[i,j]+= x[i,j]*y[i,j];
-  	}
+	    for(int j = 0; j < sizeOfC1; j=j+1){
+	      empty[i,j]+= x[i,j]*y[i,j];
+  	  }
     }
   return empty;
 }
 
 def int main(){
   
-matrix<int> m = [1,4];
-matrix<int> n = [2,3];
-matrix<int> empty = [0,0];
+  matrix<int> m = [1,4];
+  matrix<int> n = [2,3];
+  matrix<int > empty = [0,0];
 
-matrix<int> multiplied = multiply(m,n);
+  matrix<int> multiplied = multiply(m,n);
 
   printm(m);
   printm(n);
