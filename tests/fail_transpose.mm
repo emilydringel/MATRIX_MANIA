3ad21ca9e087a80ad8f3f654de6c692c446dabdc
Cindy Espinosa <cje2127@barnard.edu>
2021-04-22 20:23:09 -0400

    syntax

--- tests/fail_transpose.mm
+++ tests/fail_transpose.mm
@@ -1,23 +1,26 @@
-def matrix<int> transpose(matrix<int> m, matrix<int> n){
- matrix<int> rowscols = size(m);
+def matrix<int> transpose(matrix<int> original, matrix<int> result){
+    int sizeOfR1 = getRows(original);
+    int sizeOfC1= getColumns(original);
 
- for(int i = 0; i < rowscols[0,0]; i=i+1){
-  for(int j = 0; j < rowscols[1,0]; j=j+1)
-	n[i,j] = m[j,i];  
+    for(int i = 0; i < sizeOfR1; i=i+1){
+        for(int j = 0; j < sizeOfC1; j=j+1){
+            result[i,j] = original[j,i];
+        }
 }
- return n;
+    return result;
 }
 
 def int main(){
 
-matrix<float> m = [1,2,3; 4,5,6]; /* Fail: incompatible types */
-matrix<int> n = [0,0;0,0;0,0];
-matrix<int> tranposed = transpose(m,m);
+    matrix<float> m = [1,2,3; 4,5,6]; /* Fail: incompatible types */
+    matrix<int> n = [0,0;0,0;0,0];
+    matrix<int> tranposed = transpose(m,m);
 
 
- printm(m);
- printm(transposed);
+    printm(m);
+    printm(transposed);
 
- printm(transpose([7,8,9,10; 11,12,13,14]);
- printm(transpose([7,8,9,10; 11,12,13,14],[0,0;0,0;0,0;0,0]);
+    printm(transpose([7,8,9,10; 11,12,13,14]);
+    printm(transpose([7,8,9,10; 11,12,13,14],[0,0;0,0;0,0;0,0]);
 } 
+
