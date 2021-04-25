ad92640012c926a7961ddc1e3b87985680e60ce6
Cindy Espinosa <cje2127@barnard.edu>
2021-04-25 14:58:35 -0400

    fail test for float/ints math

---
+++ tests/fail_int_float_ops.mm
@@ -0,0 +1,24 @@
+/* operators on ints and floats */ 
+
+def void testintfloat(int a, float b){
+    print(a + b);
+    print(a - b);
+    print(a * b);
+    print(a / b);
+    print(a == b);
+    print(a == a);
+    print(a != b);
+    print(a != a);
+    print(a > b);
+    print(a >= b);
+    print(a < b);
+    print(a <= b);
+}
+def int main(){
+    
+    int c = 80;
+    float d = 5.5;
+
+    testintfloat(c, d);
+    testintfloat(d, d); */ Fail: expecting int as first argument */
+}
